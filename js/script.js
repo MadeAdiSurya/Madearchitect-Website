cbf6d647e35c9e5afa6a2189fc38240476ac3bcb
Made Adi Surya Pramana <18220105@std.stei.itb.ac.id>
2023-01-30 21:55:25 +0700

    finish main, about, portfolio

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ const pickMenu = document.querySelector(".navbar");
 document.querySelector("#humberger-menu").onclick = () => {
   pickMenu.classList.toggle("active");
 };
+
+//if click every where outside itself, navbar will be closed
+
+const pickHumMenu = document.querySelector("#humberger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!pickHumMenu.contains(e.target) && !pickMenu.contains(e.target)) {
+    pickMenu.classList.remove("active");
+  }
+});
